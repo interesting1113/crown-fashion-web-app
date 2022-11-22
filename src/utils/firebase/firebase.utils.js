bc13eaef6e0ced0720b262b12c85f7d9b2b31d1f
interesting1113 <sylviayang1113@gmail.com>
2022-11-22 20:54:40 +0900

    user doc logic

--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -1,26 +1,55 @@
-import { initializeApp } from "firebase/app";
+import { initializeApp } from 'firebase/app';
 import {
   getAuth,
   signInWithRedirect,
   signInWithPopup,
   GoogleAuthProvider,
-} from "firebase/auth";
+} from 'firebase/auth';
+import { getFirestore, doc, getDoc, setDoc } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyApN9bPvszgDlELuX5IXQSHxWnl4QWYIuw",
-  authDomain: "crown-clothing-db-1b064.firebaseapp.com",
-  projectId: "crown-clothing-db-1b064",
-  storageBucket: "crown-clothing-db-1b064.appspot.com",
-  messagingSenderId: "326043733387",
-  appId: "1:326043733387:web:68e71fcf92aaca51e3f38e",
+  apiKey: 'AIzaSyApN9bPvszgDlELuX5IXQSHxWnl4QWYIuw',
+  authDomain: 'crown-clothing-db-1b064.firebaseapp.com',
+  projectId: 'crown-clothing-db-1b064',
+  storageBucket: 'crown-clothing-db-1b064.appspot.com',
+  messagingSenderId: '326043733387',
+  appId: '1:326043733387:web:68e71fcf92aaca51e3f38e',
 };
 
 const firebaseApp = initializeApp(firebaseConfig);
 
 const provider = new GoogleAuthProvider();
 provider.setCustomParameters({
-  prompt: "select_account",
+  prompt: 'select_account',
 });
 
 export const auth = getAuth();
 export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
+
+export const db = getFirestore();
+
+export const createUserDocumentFromAuth = async (userAuth) => {
+  const userDocRef = doc(db, 'users', userAuth.uid);
+
+  const userSnapshot = await getDoc(userDocRef);
+
+  // if user data does not exists
+  // create / set the document with the data
+  if (!userSnapshot.exists()) {
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
+
+    try {
+      await setDoc(userDocRef, {
+        displayName,
+        email,
+        createdAt,
+      });
+    } catch (error) {
+      console.log('error creating the user', error.message);
+    }
+  }
+
+  // if user data exists, just return
+  return userDocRef;
+};
